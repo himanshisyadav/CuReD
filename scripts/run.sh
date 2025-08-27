44d72a05b8db4023177cc57f5cf5290c1e81f0d6
Himanshi Yadav <hyadav@midway3-login4.rcc.local>
2025-08-27 15:38:36 -0500

    Pipeline tested

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=ocr
+#SBATCH --nodes=1
+#SBATCH --time=00:30:00
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=96gb
+#SBATCH --output=./SLURM_logs/ocr_%j.out
+#SBATCH --error=./SLURM_logs/ocr_%j.err
+#SBATCH --account=rcc-staff
+#SBATCH --mail-type=END
+#SBATCH --mail-user=hyadav@rcc.uchicago.edu
+
+# Print SLURM job information
+echo "=========================================="
+echo "SLURM Job Information"
+echo "=========================================="
+echo "Job ID: $SLURM_JOB_ID"
+echo "Job Name: $SLURM_JOB_NAME"
+echo "Node List: $SLURM_JOB_NODELIST"
+echo "Number of Nodes: $SLURM_JOB_NUM_NODES"
+echo "Number of Tasks: $SLURM_NTASKS"
+echo "CPUs per Task: $SLURM_CPUS_PER_TASK"
+echo "Memory per Node: $SLURM_MEM_PER_NODE"
+echo "Partition: $SLURM_JOB_PARTITION"
+JOB_TIME_LIMIT=$(squeue -j $SLURM_JOB_ID -h --Format TimeLimit)
+echo "Time Limit: $JOB_TIME_LIMIT"
+echo "Working Directory: $SLURM_SUBMIT_DIR"
+echo "Start Time: $(date)"
+echo "=========================================="
+echo ""
+
 module load python/miniforge-25.3.0
 module load gcc/13.2.0
 
@@ -9,13 +40,22 @@ export LD_LIBRARY_PATH=/project/rcc/hyadav/lipvips-8.18.0/lib64:$LD_LIBRARY_PATH
 # Set pkg-config path (you already have this)
 export PKG_CONFIG_PATH=/project/rcc/hyadav/lipvips-8.18.0/lib64/pkgconfig:$PKG_CONFIG_PATH
 
-# Test Python import
-python -c "import pyvips; print('pyvips loaded successfully')"
+# magick -density 900 -quality 100 -colorspace Gray -background white -type Grayscale -depth 8 page-034.pdf output-%03d.png
 
-kraken -I /project/rcc/hyadav/CuReD/data/CAD/split_pages/png_tests/output-000.png -o /project/rcc/hyadav/CuReD/data/CAD/predictions/ocr.txt binarize segment ocr -m ../models/latest.mlmodel
+# kraken -I /project/rcc/hyadav/CuReD/data/CAD/split_pages/png_tests/output-000.png -o .txt binarize segment ocr -m ../models/latest.mlmodel
 
 # magick -density 600 page-034.pdf test-600.png
 # magick -density 900 page-034.pdf test-900.png
 # magick -density 1200 page-034.pdf test-1200.png
 
-# magick -density 900 -quality 100 -colorspace Gray -background white -type Grayscale -depth 8 page-034.pdf output-%03d.png
+
+for page in {37..43}; do
+    echo "Processing page $page"
+    magick -density 900 -quality 100 -colorspace Gray -background white \
+      -type Grayscale -depth 8 page-$(printf "%03d" $page).pdf temp.png
+    kraken -i temp.png -o .txt binarize segment ocr -m ../models/latest.mlmodel
+    # rm temp.png
+done
+
+echo "All pages processed!"
+
